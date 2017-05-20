7cb48fe7ef3ac8bb79997d9b023b54cfbc6e9878
karimata110611 <karimata1106@gmail.com>
2017-05-21 00:16:27 +0900

    ScrollVIew

---
+++ ScrollView/ScrollView/Views/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Xamarin.Forms;
+
+namespace ScrollView.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var scrollVIew = new Xamarin.Forms.ScrollView
+            {
+                Content = new StackLayout
+                {
+                    Spacing = 0,
+                    WidthRequest = 100,
+                    Children =
+                    {
+                        new BoxView
+                        {
+                            Color = Color.Blue,
+                            HeightRequest = 500,
+                        },
+                    
+                        new BoxView
+                        {
+                            Color = Color.Yellow,
+                            HeightRequest = 500,
+                        },
+                    
+                        new BoxView
+                        {
+                            Color = Color.Red,
+                            HeightRequest = 500,
+                        },
+                    },
+                }
+            };
+
+            //UIにStackLayoutをバインドする
+            Content = scrollVIew;
+        }
+    }
+}
